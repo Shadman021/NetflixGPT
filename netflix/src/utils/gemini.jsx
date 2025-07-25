899ba5f31bf2c9091395994d89e1778cba3e203a
Shadman021 <shadman12278@gmail.com>
2025-07-25 21:36:05 +0530

    Update gemini.jsx

--- netflix/src/utils/gemini.jsx
+++ netflix/src/utils/gemini.jsx
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyC4VYRIduD3Qu3-h4WkB94hYsSE-nlhQhM");
+
 
 export const getGeminiResponse = async (prompt) => {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
